aa87b2b6428f26dd54ffb5ac9673f6733fd6fcf5
John Carr <john.carr@unrouted.co.uk>
2016-06-06 10:48:26 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ except ImportError:
 # Take a look at http://www.python.org/dev/peps/pep-0386/
 # for a consistent versioning pattern.
 
-PSYCOPG_VERSION = '2.6.1'
+PSYCOPG_VERSION = '2.6.1.1'
 
 version_flags   = ['dt', 'dec']
 
